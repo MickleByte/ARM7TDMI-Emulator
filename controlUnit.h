eb6c6e1be1adaf28a3e4a0de3b8274a67a302316
howe-oh <32961755+howe-oh@users.noreply.github.com>
2019-03-07 05:20:55 +0000

    Update controlUnit.h

--- controlUnit.h
+++ controlUnit.h
@@ -1,14 +1,20 @@
-#include "register.h"
+#include <vector>
+#include <string>
+#include "Register.h"
+using namespace std;
 
-class ControlUnit{
+class ControlUnit {
 private:
-  Register* registerArray;
-  //0 - 30 are general-purpose registers
-  //31 - 36 are status registers
-  //Pointers are used so one array can have both general-purpose and specialised registers.
+	Register* registerArray;
+
+	//0 - 30 are general-purpose registers
+	//31 - 36 are status registers
+	//Pointers are used so one array can have both general-purpose and specialised registers.
 public:
-  void setRegister(int register, int data); //Set the value of a register
-  int getRegister(int register); //Get the value of a register
-  ControlUnit();
-  ~ControlUnit();
+	void setRegister(int register, int data); //Set the value of a register
+	int getRegister(int register); //Get the value of a register
+	ControlUnit();
+	~ControlUnit();
+	vector<string> ReadFile(string);
+	void Decode(vector<string>);
 };
