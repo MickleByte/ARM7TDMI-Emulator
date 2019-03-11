4ff0760f28b8d427e47f19bfbb58bec54996d3d8
howe-oh <32961755+howe-oh@users.noreply.github.com>
2019-03-11 16:40:25 +0000

    Update Control Unit.cpp

--- Control Unit.cpp
+++ Control Unit.cpp
@@ -63,10 +63,17 @@ void ControlUnit::Decode(string ListOfIns) {
 	}
 	SepIns[i] = op;
 
-	cout << SepIns[0] << " " << SepIns[1] << " " << SepIns[2] << endl;
-	// alu->Control(SepIns[0], stoi(SepIns[1]), stoi(SepIns[2]), registerArray[31]);
-	cout << alu->Control(SepIns[0], stoi(SepIns[1]), stoi(SepIns[2]), registerArray[31]) << endl;
-
+	if (SepIns[0] == "STR") {
+		mem[SepIns[1]] = getRegister(SepIns[2]);
+	}
+	else if (SepIns[0] == "LDR") {
+		setRegister(SepIns[2]), mem[SepIns[1]);
+	}
+	else {
+		cout << SepIns[0] << " " << SepIns[1] << " " << SepIns[2] << endl;
+		// alu->Control(SepIns[0], stoi(SepIns[1]), stoi(SepIns[2]), registerArray[31]);
+		cout << alu->Control(SepIns[0], stoi(SepIns[1]), stoi(SepIns[2]), registerArray[31]) << endl;
+	}
 }
 
 string ControlUnit::FetchNext(){
